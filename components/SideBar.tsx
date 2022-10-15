dff8f4fa0b6b88aeed85beb5ee5fb799821f74b4
AwesomeCocky <dima.naruzhniy@yandex.ru>
2022-10-15 19:24:41 +0300

    changes

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -10,7 +10,7 @@ const SideBar = () => {
       </h1>
       <nav className={styles.sideBar__navigation}>
         <h2 aria-label='home'><Link href='#'><AiOutlineHome className={styles.sideBar__icon}/></Link></h2>
-        <h2 aria-label='profile'><Link href='#'><AiOutlineUser className={styles.sideBar__icon}/></Link></h2>
+        <Link href='/profile'>1</Link>
         <h2 aria-label='watch games'><Link href='#'><AiOutlineEye className={styles.sideBar__icon}/></Link></h2>
         <h2 aria-label='learn chess'><Link href='#'><AiOutlineRead className={styles.sideBar__icon}/></Link></h2>
       </nav>
---
+++ pages/profile.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const ProfilePage: NextPage = () => {
+  return (
+    <div style={{color:'#000'}}>Profile</div>
+  )
+}
+
+export default ProfilePage
